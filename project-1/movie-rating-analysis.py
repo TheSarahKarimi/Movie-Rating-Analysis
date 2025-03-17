fc522a71ad466b456a6e14180093866a72ccc227
Sarah <TheSarahKarimi@gmail.com>
2025-03-17 23:50:41 +0330

    task-1.1-dataset-explore

--- project-1/movie-rating-analysis.py
+++ project-1/movie-rating-analysis.py
@@ -7,4 +7,14 @@ with open('movies.csv','r',encoding='utf-8') as File: # Used Python (not NumPy) 
     DataList=list(RawData) # Convert to list
 DataArray=np.array(DataList,dtype=str) # Convert to NumPy Array
 
+# Explore the dataset
+print('\n','_'*100,'\n')
+print(f'Given dataset\'s header: \n\n{DataArray[0]}')
+print('_'*100)
+print(f'The dataset has {DataArray.shape[1]} columns and {DataArray.shape[0]} rows')
+print('_'*100)
+print('A preview of dataset (first 5 rows, header exculded):\n')
+for Rows in DataArray[1:6]:
+    print(Rows,'\n')
+print('_'*100,'\n')
 
