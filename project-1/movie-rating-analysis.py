f9d50c684a31584f3ce33804d89e8966bab25cf6
Sarah <TheSarahKarimi@gmail.com>
2025-03-17 23:55:39 +0330

    task-1.1-numerical-computations

--- project-1/movie-rating-analysis.py
+++ project-1/movie-rating-analysis.py
@@ -27,3 +27,34 @@ print(f'Median of rating: {np.nanmedian(Rating):.2f}')
 print(f'Standard deviation of rating: {np.nanstd(Rating):.2f}')
 print('_'*100,'\n')
 
+# Some numerical computation with NumPy
+YearColumn=DataArray[1:,3].astype('O') # Extract the year column
+Year=np.where(YearColumn=='',np.nan,YearColumn) # Replace empty strings with 'nan'
+print('*'*10,'Some facts about this dataset:','*'*10)
+print(f'\n1.The oldest movie belong to {np.nanmin(Year)} and {np.max(Year)} is the latest release!')
+
+FirstYearIndex=np.where(Year==np.nanmin(Year)) # Fetch oldest year's movies tuple
+LastYearIndex=np.where(Year==np.nanmax(Year))# Fetch latest year's movies tuple
+FirstYearReleaseds=DataArray[FirstYearIndex[0]] # Access oldest year's movies index
+LastYearReleaseds=DataArray[LastYearIndex[0]] # Access latest year's movies index
+FirstBudgetColumn=FirstYearReleaseds[:,12] # Extract the budget value of oldest year's movies
+LastBudgetColumn=LastYearReleaseds[:,12] # Extract the budget value latest year's movies
+def to_numeric(value): # Convert to numeric, replace non-numeric with nan
+    try:
+        return int(value)  # Try converting to int
+    except ValueError:
+        return np.nan  # Return nan if conversion fails
+FirstBudget=[]
+for BudgetIndex in FirstBudgetColumn: # Allocate total budget of oldest year's movies
+    FirstNumericValue=to_numeric(BudgetIndex) 
+    FirstBudget.append(FirstNumericValue) 
+LastBudget=[]
+for BudgetIndex in LastBudgetColumn: # Allocate total budget of latest year's movies
+    LastNumericValue=to_numeric(BudgetIndex)
+    LastBudget.append(LastNumericValue) 
+print(f'\n2.On {np.nanmin(Year)}, {np.nansum(FirstBudget)} $ was spent on movies,while the budget allocated to {np.nanmin(Year)}\'s movies was {np.nansum(LastBudget)} $!')
+
+Genre=np.unique(DataArray[1:,2])
+print(f'\n3.This dataset has {np.shape(Genre)[0]} genres including:\n{Genre}')
+
+print('_'*100,'\n')
