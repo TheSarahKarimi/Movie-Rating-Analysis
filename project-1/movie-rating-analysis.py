ebc17aa9b4a82149d012adeb9a325194ea40ad3b
Sarah <TheSarahKarimi@gmail.com>
2025-03-18 03:23:12 +0330

    task-1.1-update-dataset-load

--- project-1/movie-rating-analysis.py
+++ project-1/movie-rating-analysis.py
@@ -3,7 +3,7 @@ import csv
 import pandas as pd
 
 # Load the dataset
-with open('movies.csv','r',encoding='utf-8') as File: # Used Python (not NumPy) due to quoted string support
+with open('project-1/movies.csv','r',encoding='utf-8') as File: # Used Python (not NumPy) due to quoted string support
     RawData=csv.reader(File)
     DataList=list(RawData) # Convert to list
 DataArray=np.array(DataList,dtype=str) # Convert to NumPy Array
