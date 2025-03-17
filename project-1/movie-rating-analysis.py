ecc47b2cb9f1629537c3e9fac6fa30d6a8ef5cc1
Sarah <TheSarahKarimi@gmail.com>
2025-03-17 23:54:19 +0330

    task-1.1-rating-statistics

--- project-1/movie-rating-analysis.py
+++ project-1/movie-rating-analysis.py
@@ -18,3 +18,12 @@ for Rows in DataArray[1:6]:
     print(Rows,'\n')
 print('_'*100,'\n')
 
+# Calculate basic statistics
+RatingColumn=DataArray[1:,6].astype('O') # Extract the Rating column (Converted to object to allow mixed types)
+Rating=np.where(RatingColumn=='',np.nan,RatingColumn).astype(float) # Replace empty strings with 'nan'
+print('*'*10,'Rating\'s overview:','*'*10)
+print(f'\nMean of rating: {np.nanmean(Rating):.1f}')
+print(f'Median of rating: {np.nanmedian(Rating):.2f}')
+print(f'Standard deviation of rating: {np.nanstd(Rating):.2f}')
+print('_'*100,'\n')
+
